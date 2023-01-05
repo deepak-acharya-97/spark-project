f7ad4c5d3f04fd07341be0334e5c22e653bfbb3b
Deepak Acharya <aacharyadeepak100@gmail.com>
2023-01-05 11:49:32 +0530

    Update create-topics.sh

--- spark_dashboard/kafka/create-topics.sh
+++ spark_dashboard/kafka/create-topics.sh
@@ -1,1 +1,3 @@
-
+export PATH=$PATH:/home/hadoop/kafka/bin
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic orders_data
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic orders_ten_sec_data
